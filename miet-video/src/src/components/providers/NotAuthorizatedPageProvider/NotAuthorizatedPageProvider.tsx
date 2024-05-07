c78e2627ee2ac9d62b264a6afc097885b3432a85
Dmitriy <dmitriy.korotov.76@mail.ru>
2024-05-07 09:58:38 +0300

    Fixed errors

--- miet-video/src/src/components/providers/NotAuthorizatedPageProvider/NotAuthorizatedPageProvider.tsx
+++ miet-video/src/src/components/providers/NotAuthorizatedPageProvider/NotAuthorizatedPageProvider.tsx
@@ -1,19 +1,33 @@
 'use client';
 
 import { NextPage } from "next";
+import { useEffect, useState } from "react";
 
 import NotAuthorizatedPage from "../../pages/NotAuthorizatedPage/NotAuthorizatedPage";
 
 import useAuth from "@/src/hooks/UseAuth";
+import LoadingComponent from "../../ui/LoadingComponent/LoadingComponent";
 
 
 
 const NotAuthorizatedPageProvider: NextPage<{ children: Readonly<React.ReactNode> }> = ({ children }) => {
-
+    const [ isAuntificated, setAuntificated ] = useState(false);
+    const [ isLoading, setLoading ] = useState(true);
     const { IsAuntificated } = useAuth();
 
+    useEffect(() => {
+        setAuntificated(IsAuntificated());
+        setLoading(false);
+    }, [isAuntificated, isLoading]);
+
     return (
-        IsAuntificated() ? children : <NotAuthorizatedPage/> 
+        isLoading ? 
+        <LoadingComponent/>
+        : isAuntificated ? 
+        <div onClick={event => setAuntificated(IsAuntificated())}>
+            {children}
+        </div>
+         : <NotAuthorizatedPage/> 
     );
 }
 
--- miet-video/src/src/components/ui/Header/AuthorizationBlock/AuthorizationBlock.tsx
+++ miet-video/src/src/components/ui/Header/AuthorizationBlock/AuthorizationBlock.tsx
@@ -3,30 +3,25 @@
 import useAuth from "@/src/hooks/UseAuth";
 import UnregistratedBlockBlock from "./UnregistatedBlock/UnregistatedBlock";
 import UserProfileBlock from "./UserProfileBlock/UserProfileBlock";
-import { useEffect } from "react";
-import { StudentsService } from "@/src/services/StudentsService";
-import { useAlert } from "@/src/hooks/UseAlert";
+import { useEffect, useState } from "react";
 
 
 
 const AuthorizationBlock = () => {
+    const [ isAuntificated, setAuntificated ] = useState(false);
+    const { IsAuntificated } = useAuth();
 
-    let loadedUsername: string = "";
-
-    const { IsAuntificated, GetToken } = useAuth();
-    const { Alert } = useAlert();
     useEffect(() => {
-        async function GetUsername() {
-            let maybeUserInfo = await StudentsService.GetStudentInfo(GetToken());
-            if (!maybeUserInfo.HasValue()) {
-                Alert("ERROR", maybeUserInfo.Error() || "");
-            }
-            loadedUsername = maybeUserInfo.Value()?.username || "";
-        }
-        GetUsername();
-    }, []);
+        setAuntificated(IsAuntificated())
+    }, [isAuntificated]);
+
     return (
-        IsAuntificated() ? <UserProfileBlock username={loadedUsername}/> : <UnregistratedBlockBlock/>
+        isAuntificated ? 
+        <div onClick={event => setAuntificated(IsAuntificated())}>
+            <UserProfileBlock/>    
+        </div>
+        :
+        <UnregistratedBlockBlock/>
     );
 }
 
