b5babcb71e0e7b5211fa3ab295d58980bfabdf74
Dmitriy <dmitriy.korotov.76@mail.ru>
2024-06-02 19:59:29 +0300

    Added colors

--- miet-video/next.config.mjs
+++ miet-video/next.config.mjs
@@ -1,7 +1,5 @@
 import { withNextVideo } from "next-video/process";
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    reactStrictMode: false
-}
+const nextConfig = {}
 
 export default withNextVideo(nextConfig);
